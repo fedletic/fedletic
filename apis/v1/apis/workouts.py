9a2d017ca6a34ca9abaffb7a1a9c5d8c4dc6c6e0
Brian de Heus <me@brian.jp>
2025-03-13 20:43:02 +0400

    Removes a cheeky print

--- apis/v1/apis/workouts.py
+++ apis/v1/apis/workouts.py
@@ -9,5 +9,4 @@ class WorkoutsAPI(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
 
     def create(self, request, *args, **kwargs):
-        print(request.FILES)
         return Response({"foo": 1})
