97e60d6bb4c96d1d8900ff41b5d51e914a7a192a
Brian de Heus <me@brian.jp>
2025-05-11 12:56:53 +0400

    Adds mailer stuff

---
+++ fedletic/mail.py
@@ -0,0 +1,4 @@
+WELCOME_SUBJECT = "Welcome to Fedletic"
+CONFIRM_SUBJECT = "Confirm your Fedletic account"
+PASSWORD_FORGET_SUBJECT = "Forgot your Fedletic password?"
+PASSWORD_RESET_SUBJECT = "Your password has been reset"
